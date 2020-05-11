5c7460949b2754a08212abe1a34c390ee753d243
Vitor <vitorrubim1@hotmail.com>
2020-05-11 19:08:26 -0300

    Ignorando Warnings

--- mobile/App.js
+++ mobile/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { YellowBox } from 'react-native';
 import Routes from './src/routes';
+
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket connection',
+  'Remote debugger'
+])
 
 export default function App() {
   return <Routes />
