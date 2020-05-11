1c87c945d2b6bf21b4528e9b318cb81453ba8708
Vitor <vitorrubim1@hotmail.com>
2020-05-11 19:07:58 -0300

    feat: Adicionado a estes controllers a função do dono do spot recusar e aceitar visita, comunicando com o mobile

--- backend/src/controllers/RejectionController.js
+++ backend/src/controllers/RejectionController.js
@@ -11,6 +11,15 @@ module.exports = {
 
         await booking.save();
 
+        //PRA ENVIAR PRO MOBILE
+
+        //VERIFICANDO O USUÁRIO QUE ESTA FAZENDO A RESERVA
+        const bookingUserSocket = req.connectedUsers[booking.user];
+
+        if(bookingUserSocket){
+            req.io.to(bookingUserSocket).emit('booking_response', booking); 
+        }
+
         return res.json(booking);
     }
 };
